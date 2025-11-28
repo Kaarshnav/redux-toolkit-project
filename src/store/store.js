ab3ad8617e2828c0adbd7053de84ec54f0f16a1a
Kaarshnav <rajneeshupreti98@gmail.com>
2025-11-29 02:58:47 +0530

    ✅ adding persistance to redux toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import toDoReducer from "../store/slices/toDoSlice";
-const store = configureStore({
-  reducer: { toDo: toDoReducer },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage for web
+
+const rootReducer = combineReducers({
+  toDo: toDoReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["toDo"], // only persist this slice
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // required for redux-persist
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
